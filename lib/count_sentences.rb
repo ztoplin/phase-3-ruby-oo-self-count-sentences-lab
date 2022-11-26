836993cf68ffcc2af892861895750ce157f55678
Aaron <ztoplin@yahoo.com>
2022-11-26 16:19:36 -0500

    commit tree b7163c1ce378671d348a19f61854dad923f8b8fa

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
+    # non-regular expression solution:
+    # sentences = 0
+    # # what punchtuation we're checking for 
+    # punctuation = [".", "?", "!"]
+    # # iterate over every character in the string 
+    # self.chars.each.with_index do |char, index|
+    # # check if the character is a punctuation, and if the _next_ character is _not_ a punctuation 
+    # if punctuation.include?(char) && !punctuation.include?(self[index + 1])
+    #  sentences +=1
+    # end
+    # sentences
 
+    # regular expression solution
+    # /\.|\?|!/ is a regular expression that searches for periods, qustion marks and exclamations in a string 
+    self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.size
   end
 end
